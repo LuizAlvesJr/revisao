d7b512c94ee926c4b51b4f6f6124846c15c5e72b
luiz <luiz@PcGamer>
2020-03-29 23:05:33 -0300

    Teste sync

---
+++ primeiro-programa/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,20 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+
+		Aluno aluno1 = new Aluno();
+		
+		
+		Aluno aluno2 =  new Aluno();
+		
+		Aluno aluno4 = new Aluno("Jose", 50);
+		
+		System.out.println(aluno2);
+	} 
+	
+
+}
